4ba2d4cf556780be3fbcc20d81d2171954cf340d
Nikita Kirillov <nikitasidorow1@gmail.com>
2023-12-25 12:28:29 +0300

    First step to create

--- simpleML/main.cpp
+++ simpleML/main.cpp
@@ -20,5 +20,55 @@ public:
 	int layers;
 	neuron** neurons;
 	double*** weights;
+	int* size;
+	int threadsNum;
 
+	double sign_pro(double x)
+	{
+		if ((fabs(x - 1) < 1e-9) || (fabs(x) < 1e-9)) return 0.0;
+		double res = x * (1.0 - x);
+		return res;
+	}
+
+	double predict(double x)
+	{
+		if (x >= 0.8)
+			return 1;
+		else
+			return 0;
+	}
+
+	void setLayers(int n, int* p)
+	{
+		srand(time(0));
+		layers = n;
+		neurons = new neuron * [n];
+		weights = new double** [n - 1];
+		size = new int[n];
+		for (int i(0); i < n; i++)
+		{
+			size[i] = p[i];
+			neurons[i] = new neuron[p[i]];
+			if (i < n - 1)
+			{
+				weights[i] = new double* [p[i]];
+				for (int j(0); j < p[i]; j++)
+				{
+					weights[i][j] = new double[p[i + 1]];
+					for (int k(0); k < p[i + 1]; k++)
+					{
+						weights[i][j][k] = ((rand() % 100)) * 0.01 / size[i];
+					}
+				}
+			}
+		}
+	}
+
+	void set_input(double* p)
+	{
+		for (int i = 0; i < size[0]; i++)
+		{
+			neurons[0][i].value = p[i];
+		}
+	}
 };
